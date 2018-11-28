cd859ae19e5f158b1f6706c27a3785989472679d
lingtao13 <nelsonpeng13@gmail.com>
2018-11-28 11:19:38 +0800

    just test new machine

--- 6_line.py
+++ 6_line.py
@@ -1,10 +1,11 @@
 import itertools
 
 list1 = (1)
-for i in itertools.permutations([2,3,4,5,6,7,8,9,10,11,12]):
-     if 1+i[1]+i[4]+i[6]==26 and 1+i[2]+i[5]+i[9]==26 and i[6]+i[7]+i[8]+i[9]==26 and i[0]+i[4]+i[7]+i[10]==26 and \
-        i[0] + i[1] + i[2] + i[3] == 26 and i[3] + i[5] + i[8] + i[10] == 26:
-         print(i)
-
-
-
+count = 0
+for i in itertools.permutations([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]):
+    if 1 + i[1] + i[4] + i[6] == 26 and 1 + i[2] + i[5] + i[9] == 26 and i[6] + i[7] + i[8] + i[9] == 26 and i[0] + i[
+        4] + i[7] + i[10] == 26 and \
+            i[0] + i[1] + i[2] + i[3] == 26 and i[3] + i[5] + i[8] + i[10] == 26:
+        print(i)
+        count += 1
+print(count)
--- multiprocess/Mulpro_test2.py
+++ multiprocess/Mulpro_test2.py
@@ -3,11 +3,10 @@
 __author__ = 'nelson'
 __date__ = '2018/8/31 上午9:53'
 
-
-from multiprocessing import Process,Pool
-import datatime,random,sys,os,pymysql
 from datetime import datetime
-from time import sleep
+from multiprocessing import Pool
+
+import pymysql
 
 conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123', db='queue_thread')
 cur = conn.cursor()
@@ -15,32 +14,32 @@ sql = "insert into threadtest(id,id2,id3) VALUES (%s,%s,%s)"
 
 
 def mysql_insert(x):
-    cur.execute(sql,x)
+    cur.execute(sql, x)
 
 
 def read_line(lines, mysql_insert):
-    insertlist=[]
+    insertlist = []
     for i in lines:
         insertlist.append(i.split('\t'))
     mysql_insert(insertlist)
 
 
 if __name__ == "__main__":
-    f=open('/Users/nelsonpeng/Downloads/qtfyt.txt')
-    p=Pool(10)
+    f = open('/Users/nelsonpeng/Downloads/qtfyt.txt')
+    p = Pool(10)
     start = datetime.now()
-    count=0
+    count = 0
     contentlist = []
     while True:
-        count+=1
-        content=f.readline()
-        if content=="":
+        count += 1
+        content = f.readline()
+        if content == "":
             break
-        p.apply_async(mysql_insert,args=(content.split('\t'),))
-        if count%100==0:
+        p.apply_async(mysql_insert, args=(content.split('\t'),))
+        if count % 100 == 0:
             print(count)
     p.close()
     p.join()
     conn.commit()
     end = datetime.now()
-    print("dequeue done and take time %s"%(end-start))
+    print("dequeue done and take time %s" % (end - start))
